5f77e2dce2e0f4554d4ed9e91c6049a9fa3ce461
mixed_nuts_nuts <french0719@gmail.com>
2022-05-31 20:53:27 +0900

    chapter6

--- chapter6/contracts/Fundraiser.sol
+++ chapter6/contracts/Fundraiser.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
 // 新しい資金調達を作成するのに必要なもの　↓ (状態変数)
 
 // 受取人の名前
@@ -9,17 +10,30 @@ pragma solidity >=0.4.22 <0.9.0;
 // 受取人に関する簡単な説明
 // 管理人またはownerのアドレス
 
-contract FundRaiser {   
+contract FundRaiser is Ownable{   
     // contract に状態変数の追加
     string public name;
     string public url;
     string public imageURL;
     string public description;
-    constructor (string memory _name, string memory _url, string memory _imageURL, string memory _description) public {
+    address payable public beneficiary;
+    constructor (string memory _name, 
+                string memory _url, 
+                string memory _imageURL,
+                string memory _description, 
+                address payable _beneficiary,
+                address _custodian
+                ) public 
+                {
         name = _name;
         url = _url;
-        imageURL = imageURL;
+        imageURL = _imageURL;
         description = _description;
+        beneficiary = _beneficiary;
+        transferOwnership(_custodian);
+    }
+    function setBeneficiary (address payable _beneficiary) public onlyOwner {
+        beneficiary = _beneficiary;
     }
 }
 
--- chapter6/test/fundraiser.js
+++ chapter6/test/fundraiser.js
@@ -6,17 +6,22 @@ contract("Fundraiser", (accounts) => {
   const url = "beneficiaryname.org";
   const imageURL = "https://placekitten.com/600/350";
   const description = "Beneficiary description";
+  const beneficiary = accounts[1];
+  const owner = accounts[0];
+
+  // beforeEach .. 各テストサンプルが実行される前に資金調達を設定。
+  beforeEach(async () => {
+    fundraiser = await FundraiserContract.new(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      owner
+    );
+  });
 
   describe("initialization", () => {
-    // beforeEach .. 各テストサンプルが実行される前に資金調達を設定。
-    beforeEach(async () => {
-      fundraiser = await FundraiserContract.new(
-        name,
-        url,
-        imageURL,
-        description
-      );
-    });
     // getter で　データが正しく設定されているか確認
     it("gets the beneficiary name", async () => {
       const actual = await fundraiser.name();
@@ -26,13 +31,44 @@ contract("Fundraiser", (accounts) => {
       const actual = await fundraiser.url();
       assert.equal(actual, url, "url should match");
     });
-    // it("gets the beneficiary image url", async () => {
-    //   const actual = await fundraiser.imageURL();
-    //   assert.equal(actual, imageURL, "imageURL should match");
-    // });
+    it("gets the beneficiary image url", async () => {
+      const actual = await fundraiser.imageURL();
+      assert.equal(actual, imageURL, "imageURL should match");
+    });
     it("gets the beneficiary description", async () => {
       const actual = await fundraiser.description();
       assert.equal(actual, description, "description should match");
+    });
+    it("gets the beneficiary", async () => {
+      const actual = await fundraiser.beneficiary();
+      assert.equal(actual, beneficiary, "beneficiary should match");
+    });
+    it("gets the beneficiary owner", async () => {
+      const actual = await fundraiser.owner();
+      assert.equal(actual, owner, "owner should match");
+    });
+  });
+  describe("setBeneficiary", () => {
+    const newBeneficiary = accounts[2];
+
+    it("updated beneficiary when called by owner account", async () => {
+      await fundraiser.setBeneficiary(newBeneficiary, { from: owner });
+      const actualBeneficiary = await fundraiser.beneficiary();
+      assert.equal(
+        actualBeneficiary,
+        newBeneficiary,
+        "beneficiary should match"
+      );
+    });
+    it("throws and error when called from a non-owner account", async () => {
+      try {
+        await fundraiser.setBeneficiary(newBeneficiary, { from: accounts[3] });
+        assert.fail("withdraw was not restricted to owners");
+      } catch (err) {
+        const expectedError = "Ownable: caller is not the owner";
+        const actualError = err.reason;
+        assert.equal(actualError, expectedError, "should not be permitted");
+      }
     });
   });
 });
